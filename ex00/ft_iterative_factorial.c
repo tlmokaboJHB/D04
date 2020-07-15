cbc628e131987538620cf25d23c6c87971221070
Tlotlo Mokabo <tlmokabo@c4r5s3.wethinkcode.co.za>
2020-07-15 10:14:07 +0200

    day04

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nbr)
+{
+	if(nbr < 0)
+	{
+		nbr = nbr * -1;
+		ft_putchar('-');
+	}
+	if(nbr <= 9)
+	{
+		ft_putchar(nbr + 48);
+	}
+	else
+	{
+		ft_putnbr(nbr/10);
+		ft_putchar(nbr % 10 + 48);
+	}
+}
+int	ft_iterative_factorial(int nb)
+{ 
+	int fact;
+	int i;
+
+	i = 1;
+	fact = nb;
+	while(nb > 1)
+	{
+		fact = fact * (nb - 1); 
+			nb--;
+		//ft_putnbr(fact);
+		//ft_putchar('\n');
+		i++;
+	}
+	return fact;
+}
